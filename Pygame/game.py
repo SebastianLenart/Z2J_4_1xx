7f306fd9f341865f383338d3f92a5e44ccd459b3
SebastianLenart <stentbike7@gmail.com>
2023-04-20 21:42:20 +0200

    niewiele

--- Pygame/game.py
+++ Pygame/game.py
@@ -12,16 +12,16 @@ class Game:
                                                self.settings.screen_height))
         pygame.display.set_caption("Game: rabbits")
         self.start_ticks = pygame.time.get_ticks()  # starter tick
+        self.before_time = 0
+        print("time:", self.start_ticks)
         self.list_of_rabbits = []
         self.list_of_rabbits.append(Rabbit(self))
-
-
-        display = Display(self)
+        self.display = Display(self)
 
     def run(self):
         while True:
             self._check_events()
-            self.update_time() # moze bedzie dzialac bez tego, w sensie ze w tle
+            self.update_time()  # moze bedzie dzialac bez tego, w sensie ze w tle
             self.update_screen()
 
     def _check_events(self):
@@ -36,17 +36,17 @@ class Game:
                     self.list_of_rabbits.pop()
                     self.list_of_rabbits.append(Rabbit(self))
 
-    def check_pos_any_rabbit(self, pos):
-        pass
-
     def update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.list_of_rabbits[-1].blitme()
         pygame.display.flip()  # odswiezanie okna
 
-
     def update_time(self):
         seconds = int((pygame.time.get_ticks() - self.start_ticks) / 1000)  # calculate how many seconds
+        if self.before_time != seconds:
+            print(seconds)
+            self.before_time = seconds
+
         return seconds
 
 
