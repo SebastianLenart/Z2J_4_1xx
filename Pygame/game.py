5abbb168a42e88323304167b67d2c4a318471c1f
SebastianLenart <stentbike7@gmail.com>
2023-04-23 21:40:00 +0200

    niewielev2

--- Pygame/game.py
+++ Pygame/game.py
@@ -2,7 +2,7 @@ import pygame
 from settings import Settings
 from display import Display
 from rabbit import Rabbit
-
+from button import Button
 
 class Game:
     def __init__(self):
@@ -11,17 +11,21 @@ class Game:
         self.screen = pygame.display.set_mode((self.settings.screen_width,
                                                self.settings.screen_height))
         pygame.display.set_caption("Game: rabbits")
-        self.start_ticks = pygame.time.get_ticks()  # starter tick
+        self.start_ticks = pygame.time.get_ticks()
         self.before_time = 0
+        self.seconds = 0
         print("time:", self.start_ticks)
         self.list_of_rabbits = []
         self.list_of_rabbits.append(Rabbit(self))
         self.display = Display(self)
+        self.play_button = Button(self, "Gra")
+        self.game_active = False
 
     def run(self):
         while True:
             self._check_events()
-            self.update_time()  # moze bedzie dzialac bez tego, w sensie ze w tle
+            if self.game_active:
+                self.update_time()
             self.update_screen()
 
     def _check_events(self):
@@ -31,26 +35,28 @@ class Game:
                 exit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_pos = pygame.mouse.get_pos()
-                print(mouse_pos)
                 if self.list_of_rabbits[-1].check_pos_any_rabbit(mouse_pos):
                     self.list_of_rabbits.pop()
                     self.list_of_rabbits.append(Rabbit(self))
+                    self.start_ticks = pygame.time.get_ticks()
 
     def update_screen(self):
+        if not self.game_active:
+            self.play_button.draw_button()
+            self.game_active = True
+            return
         self.screen.fill(self.settings.bg_color)
         self.list_of_rabbits[-1].blitme()
         pygame.display.flip()  # odswiezanie okna
 
     def update_time(self):
-        seconds = int((pygame.time.get_ticks() - self.start_ticks) / 1000)  # calculate how many seconds
-        if self.before_time != seconds:
-            print(seconds)
-            self.before_time = seconds
-
-        return seconds
+        miliseconds = int((pygame.time.get_ticks() - self.start_ticks) / 1000)  # calculate how many seconds
+        if self.before_time != miliseconds:
+            self.seconds = miliseconds
+            print(self.seconds)
+            self.before_time = miliseconds
 
 
 if __name__ == "__main__":
-    # utworzenie egzemplarza gry i jej uruchomienie
     game = Game()
     game.run()
